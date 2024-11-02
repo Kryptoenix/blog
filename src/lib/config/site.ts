9f2cccf01845743e131fd8deffc71a14fe6a677a
Kryptoenix <Kryptoenix@example.com>
2024-11-03 00:05:44 +0200

    test update

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -8,7 +8,7 @@ export const site: SiteConfig = {
     status: '🌸',
   },
   description: 'Powered by SvelteKit/Urara',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
+  domain: `${import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app'}/blog`,
   lang: 'en-US',
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   subtitle: 'Sweet & Powerful SvelteKit Blog Template',
